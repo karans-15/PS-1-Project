8dbdfea2650c485a3ccb753516a5cb7c199446e2
karans-15 <67215528+karans-15@users.noreply.github.com>
2020-07-14 08:32:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ScreenNav from "./ScreenNav";
+
+function App() {
+  return (
+    <div className="App">
+      <ScreenNav></ScreenNav>
+    </div>
+  );
+}
+
+export default App;
